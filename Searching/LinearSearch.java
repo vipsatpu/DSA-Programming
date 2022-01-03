7758084696f0ee77c64f055ffd5aef8cae402d93
Vipul Satpute <vipul.satpute@t-systems.com>
2022-01-03 08:43:20 +0530

    LinearSearch

---
+++ Searching/LinearSearch.java
@@ -0,0 +1,39 @@
+// Java code for linearly searching x in arr[]. If x
+// is present then return its location, otherwise
+// return -1
+
+class GFG
+{
+    public static int search(int arr[], int x)
+    {
+        int n = arr.length;
+        for (int i = 0; i < n; i++)
+        {
+            if (arr[i] == x)
+                return i;
+        }
+        return -1;
+    }
+
+    // Driver code
+    public static void main(String args[])
+    {
+        int arr[] = { 2, 3, 4, 10, 40 };
+        int x = 10;
+
+        // Function call
+        int result = search(arr, x);
+        if (result == -1)
+            System.out.print(
+                    "Element is not present in array");
+        else
+            System.out.print("Element is present at index "
+                    + result);
+    }
+}
+
+/**
+The time complexity of the above algorithm is O(n).
+Linear search is rarely used practically because other search algorithms such as the binary search algorithm
+ and hash tables allow significantly faster-searching comparison to Linear search.
+ **/
